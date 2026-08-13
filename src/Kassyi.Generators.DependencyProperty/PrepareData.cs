d4f617682f4fb1684ffc748019dd5939af17b2f2
kassyi <22139176+kassyi@users.noreply.github.com>
2026-08-14 03:03:21 +0900

    refactor(prepare): メソッドシグネチャ検証の分割と型判定の安全性を向上
    - 変更内容の詳細:
      - CheckMethodsDirectly 内の多重分岐を CheckHas1, CheckHas2, CheckHas3 の個別ヘルパーメソッドへ抽出
      - IsEventArgsType の判定を Contains から EndsWith("EventArgs") / EndsWith("RoutedEventArgs") に厳格化
      - DependencyPropertyDataBuilder を用いたモデル生成パイプラインへ移行
    - 技術的背景:
      - メソッド解析ロジックの認知的複雑度を下げ、ユーザー定義型名による誤判定リスクを排除して保守性を高めるため

--- src/Kassyi.Generators.DependencyProperty/PrepareData.cs
+++ src/Kassyi.Generators.DependencyProperty/PrepareData.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections.Immutable;
-using System.ComponentModel;
 using Kassyi.Generators.DependencyProperty.Models;
 using Kassyi.Generators.Extensions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Immutable;
 
 
 
@@ -23,187 +22,23 @@ public static class PrepareData
     {
         attribute = attribute ?? throw new ArgumentNullException(nameof(attribute));
 
-        var name =
-            attribute.ConstructorArguments.ElementAtOrDefault(0).Value?.ToString() ??
-            string.Empty;
-        var typeSymbol =
-            attribute.GetGenericTypeArgument(0) ??
-            attribute.ConstructorArguments.ElementAtOrDefault(1).Value as ITypeSymbol;
-        var type = typeSymbol?.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat) ?? string.Empty;
-        var shortType = typeSymbol?.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat) ?? string.Empty;
-        var isValueType = typeSymbol?.IsValueType ?? true;
-        var isSpecialType = typeSymbol.IsSpecialType() ?? false;
-        var defaultValue =
-            attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.DefaultValueExpression)).Value?.ToString() ??
-            attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.DefaultValue)).Value?.ToString();
-        var defaultValueDocumentation =
-            attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.DefaultValueExpression)).Value?.ToString() ??
-            attributeSyntax?.GetNamedArgumentExpression(nameof(DependencyPropertyAttribute.DefaultValue));
-
-        defaultValue = ExpandDefaultValueExpression(defaultValue, typeSymbol);
-        defaultValueDocumentation = ExpandDefaultValueExpression(defaultValueDocumentation, typeSymbol);
-        var browsableForType =
-            attribute.GetGenericTypeArgumentOrNamed(position: 1, nameof(AttachedDependencyPropertyAttribute.BrowsableForType))?
-                .ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
-        var fromType =
-            attribute.GetGenericTypeArgumentOrNamed(position: 1, nameof(AddOwnerAttribute.FromType))?
-                .ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
-        var isReadOnly = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.IsReadOnly)).ToBoolean();
-        var isDirect = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.IsDirect)).ToBoolean();
-
-        var description = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.Description)).Value?.ToString();
-        var category = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.Category)).Value?.ToString();
-        var typeConverter = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.TypeConverter)).Value
-            ?.ToString();
-        var bindable = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.Bindable)).ToNullableBoolean();
-        var browsable = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.Browsable)).ToNullableBoolean();
-        var designerSerializationVisibility = attribute
-            .GetNamedArgument(nameof(DependencyPropertyAttribute.DesignerSerializationVisibility))
-            .ToEnum<DesignerSerializationVisibility>()?
-            .ToString("G");
-        var clsCompliant = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.ClsCompliant))
-            .ToNullableBoolean();
-        var localizability = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.Localizability))
-            .ToEnum<Localizability>()?
-            .ToString("G");
-
-        var xmlDocumentation = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.XmlDocumentation)).Value
-            ?.ToString();
-        var propertyXmlDocumentation = attribute
-            .GetNamedArgument(nameof(DependencyPropertyAttribute.PropertyXmlDocumentation)).Value?.ToString();
-        var getterXmlDocumentation = attribute
-            .GetNamedArgument(nameof(AttachedDependencyPropertyAttribute.GetterXmlDocumentation)).Value?.ToString();
-        var setterXmlDocumentation = attribute
-            .GetNamedArgument(nameof(AttachedDependencyPropertyAttribute.SetterXmlDocumentation)).Value?.ToString();
-        var bindEvent = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.BindEvent)).Value?.ToString();
-        var bindEvents = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.BindEvents));
-        var onChanged = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.OnChanged)).Value?.ToString();
-
-        var affectsMeasure = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.AffectsMeasure)).ToBoolean();
-        var affectsArrange = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.AffectsArrange)).ToBoolean();
-        var affectsParentMeasure = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.AffectsParentMeasure))
-            .ToBoolean();
-        var affectsParentArrange = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.AffectsParentArrange))
-            .ToBoolean();
-        var affectsRender = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.AffectsRender)).ToBoolean();
-        var inherits = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.Inherits)).ToBoolean();
-        var overridesInheritanceBehavior = attribute
-            .GetNamedArgument(nameof(DependencyPropertyAttribute.OverridesInheritanceBehavior)).ToBoolean();
-        var notDataBindable =
-            attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.NotDataBindable)).ToBoolean();
-        var journal = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.Journal)).ToBoolean();
-        var subPropertiesDoNotAffectRender = attribute
-            .GetNamedArgument(nameof(DependencyPropertyAttribute.SubPropertiesDoNotAffectRender)).ToBoolean();
-        var isAnimationProhibited =
-            attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.IsAnimationProhibited)).ToBoolean();
-        var defaultUpdateSourceTrigger = attribute
-            .GetNamedArgument(nameof(DependencyPropertyAttribute.DefaultUpdateSourceTrigger))
-            .ToEnum<SourceTrigger>()?
-            .ToString("G");
-        var defaultBindingMode = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.DefaultBindingMode))
-            .ToEnum<DefaultBindingMode>()?
-            .ToString("G");
-        var enableDataValidation = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.EnableDataValidation))
-            .ToBoolean();
-        var coerce = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.Coerce)).ToBoolean();
-        var validate = attribute.GetNamedArgument(nameof(DependencyPropertyAttribute.Validate)).ToBoolean();
-        var createDefaultValueCallback = attribute
-            .GetNamedArgument(nameof(DependencyPropertyAttribute.CreateDefaultValueCallback)).ToBoolean();
-
-        var isCustomOnChanged = !string.IsNullOrWhiteSpace(onChanged);
-        var onChangedName = isCustomOnChanged ? onChanged! : $"On{name}Changed";
-        var onChangingName = $"On{name}Changing";
-
-        var targetType = type.Replace("global::", string.Empty).Replace("?", string.Empty);
-        var targetSenderType = classSymbol != null
-            ? (isAttached
-                ? (browsableForType ?? GenerateDependencyObjectType(framework))
-                : classSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat))
-                .Replace("global::", string.Empty).Replace("?", string.Empty)
-            : string.Empty;
-
-        var (c0, c1, c2, c3, ca1, ca2) = classSymbol != null
-            ? CheckMethodsDirectly(classSymbol, onChangedName, targetType, targetSenderType)
-            : (false, false, false, false, false, false);
-
-        var (ch0, ch1, ch2, ch3, _, _) = classSymbol != null
-            ? CheckMethodsDirectly(classSymbol, onChangingName, targetType, targetSenderType)
-            : (false, false, false, false, false, false);
-
-        var bindEventsArray = (bindEvent != null
-            ? [bindEvent]
-            : bindEvents.Kind == TypedConstantKind.Array
-                ? bindEvents.Values
-                    .Select(static value => value.Value?.ToString() ?? string.Empty)
-                    .Where(value => !string.IsNullOrWhiteSpace(value))
-                    .ToArray()
-                : Array.Empty<string>()).ToImmutableArray().AsEquatableArray();
-
-        c2 |= !isCustomOnChanged && !isAttached && !bindEventsArray.IsEmpty;
-        c3 |= !isCustomOnChanged && isAttached && !bindEventsArray.IsEmpty;
-
-        return new DependencyPropertyData(
-            Name: name,
-            Type: type,
-            Version: version,
-            ShortType: shortType,
-            IsValueType: isValueType,
-            IsSpecialType: isSpecialType,
-            DefaultValue: defaultValue,
-            DefaultValueDocumentation: defaultValueDocumentation,
-            IsReadOnly: isReadOnly,
-            IsDirect: isDirect,
-            IsAttached: isAttached,
-            IsAddOwner: isAddOwner,
-            Framework: framework,
-            Description: description,
-            Category: category,
-            TypeConverter: typeConverter,
-            Bindable: bindable,
-            Browsable: browsable,
-            DesignerSerializationVisibility: designerSerializationVisibility,
-            ClsCompliant: clsCompliant,
-            Localizability: localizability,
-            BrowsableForType: browsableForType,
-            FromType: fromType,
-            XmlDocumentation: xmlDocumentation,
-            GetterXmlDocumentation: getterXmlDocumentation ?? propertyXmlDocumentation,
-            SetterXmlDocumentation: setterXmlDocumentation,
-            BindEvents: bindEventsArray,
-            OnChanged: onChanged ?? string.Empty,
-            AffectsMeasure: affectsMeasure,
-            AffectsArrange: affectsArrange,
-            AffectsParentMeasure: affectsParentMeasure,
-            AffectsParentArrange: affectsParentArrange,
-            AffectsRender: affectsRender,
-            Inherits: inherits,
-            OverridesInheritanceBehavior: overridesInheritanceBehavior,
-            NotDataBindable: notDataBindable,
-            Journal: journal,
-            SubPropertiesDoNotAffectRender: subPropertiesDoNotAffectRender,
-            IsAnimationProhibited: isAnimationProhibited,
-            DefaultUpdateSourceTrigger: defaultUpdateSourceTrigger,
-            DefaultBindingMode: defaultBindingMode,
-            EnableDataValidation: enableDataValidation,
-            Coerce: coerce,
-            Validate: validate,
-            CreateDefaultValueCallback: createDefaultValueCallback,
-            IsChanged0: c0,
-            IsChanged1: c1,
-            IsChanged2: c2,
-            IsChanged3: c3,
-            IsChangedArgs1: ca1,
-            IsChangedArgs2: ca2,
-            IsChanging0: ch0,
-            IsChanging1: ch1,
-            IsChanging2: ch2,
-            IsChanging3: ch3);
+        return new DependencyPropertyDataBuilder()
+            .WithCoreProperties(attribute, framework, version, isAddOwner, isAttached)
+            .WithMetadata(attribute)
+            .WithDefaultValues(attribute, attributeSyntax)
+            .WithXmlDocumentation(attribute)
+            .WithCallbacks(attribute, classSymbol)
+            .Build();
     }
 
-    private static (bool Has0, bool Has1, bool Has2, bool Has3, bool HasArgs1, bool HasArgs2) CheckMethodsDirectly(
+    internal static readonly SymbolDisplayFormat TypeFormat = SymbolDisplayFormat.FullyQualifiedFormat
+        .WithGlobalNamespaceStyle(SymbolDisplayGlobalNamespaceStyle.Omitted);
+
+    internal static (bool Has0, bool Has1, bool Has2, bool Has3, bool HasArgs1, bool HasArgs2) CheckMethodsDirectly(
         INamedTypeSymbol classSymbol, string methodName, string targetType, string senderType)
     {
-        bool has0 = false, has1 = false, has2 = false, has3 = false, hasArgs1 = false, hasArgs2 = false;
+        bool has0 = false, has1 = false, has2 = false, has3 = false;
+        bool hasArgs1 = false, hasArgs2 = false;
 
         foreach (var member in classSymbol.GetMembers(methodName))
         {
@@ -218,45 +53,16 @@ public static class PrepareData
                 case 0:
                     has0 = true;
                     break;
-
                 case 1:
-                    var type0 = GetNormalizedTypeName(p[0].Type);
-                    if (type0 == targetType || type0 == senderType)
-                    {
-                        has1 = true;
-                    }
-
-                    if (IsEventArgsType(p[0].Type.Name))
-                    {
-                        hasArgs1 = true;
-                    }
-
+                    has1 |= CheckHas1(p, targetType, senderType);
+                    hasArgs1 |= IsEventArgsType(p[0].Type.Name);
                     break;
-
                 case 2:
-                    var type02 = GetNormalizedTypeName(p[0].Type);
-                    var type12 = GetNormalizedTypeName(p[1].Type);
-                    if ((type02 == targetType && type12 == targetType) || (type02 == senderType && type12 == targetType))
-                    {
-                        has2 = true;
-                    }
-
-                    if (IsEventArgsType(p[1].Type.Name))
-                    {
-                        hasArgs2 = true;
-                    }
-
+                    has2 |= CheckHas2(p, targetType, senderType);
+                    hasArgs2 |= IsEventArgsType(p[1].Type.Name);
                     break;
-
                 case 3:
-                    var type03 = GetNormalizedTypeName(p[0].Type);
-                    var type13 = GetNormalizedTypeName(p[1].Type);
-                    var type23 = GetNormalizedTypeName(p[2].Type);
-                    if (type03 == senderType && type13 == targetType && type23 == targetType)
-                    {
-                        has3 = true;
-                    }
-
+                    has3 |= CheckHas3(p, targetType, senderType);
                     break;
             }
         }
@@ -264,18 +70,40 @@ public static class PrepareData
         return (has0, has1, has2, has3, hasArgs1, hasArgs2);
     }
 
-    private static string GetNormalizedTypeName(ITypeSymbol typeSymbol) =>
-        typeSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)
-            .Replace("global::", string.Empty)
-            .TrimEnd('?');
+    private static bool CheckHas1(ImmutableArray<IParameterSymbol> parameters, string targetType, string senderType)
+    {
+        var type0 = GetNormalizedTypeName(parameters[0].Type);
+        return type0 == targetType || type0 == senderType;
+    }
+
+    private static bool CheckHas2(ImmutableArray<IParameterSymbol> parameters, string targetType, string senderType)
+    {
+        var type0 = GetNormalizedTypeName(parameters[0].Type);
+        var type1 = GetNormalizedTypeName(parameters[1].Type);
+        return (type0 == targetType || type0 == senderType) && type1 == targetType;
+    }
+
+    private static bool CheckHas3(ImmutableArray<IParameterSymbol> parameters, string targetType, string senderType)
+    {
+        var type0 = GetNormalizedTypeName(parameters[0].Type);
+        var type1 = GetNormalizedTypeName(parameters[1].Type);
+        var type2 = GetNormalizedTypeName(parameters[2].Type);
+        return type0 == senderType && type1 == targetType && type2 == targetType;
+    }
+
+    private static string GetNormalizedTypeName(ITypeSymbol typeSymbol)
+    {
+        var str = typeSymbol.ToDisplayString(TypeFormat);
+        return str.EndsWith("?", StringComparison.Ordinal) ? str.Substring(0, str.Length - 1) : str;
+    }
 
     private static bool IsEventArgsType(string typeName) =>
         typeName.EndsWith("EventArgs", StringComparison.Ordinal) ||
         typeName.EndsWith("EventArgs>", StringComparison.Ordinal) ||
-        typeName.Contains("DependencyPropertyChangedEventArgs") ||
-        typeName.Contains("ValueChangedEventArgs");
+        typeName.EndsWith("DependencyPropertyChangedEventArgs", StringComparison.Ordinal) ||
+        typeName.EndsWith("ValueChangedEventArgs", StringComparison.Ordinal);
 
-    private static string GenerateDependencyObjectType(Framework framework) =>
+    internal static string GenerateDependencyObjectType(Framework framework) =>
         framework == Framework.Maui ? "Microsoft.Maui.Controls.BindableObject" :
         framework == Framework.Avalonia ? "Avalonia.AvaloniaObject" :
         framework == Framework.Wpf ? "System.Windows.DependencyObject" :
@@ -348,7 +176,7 @@ public static class PrepareData
             Framework: framework);
     }
 
-    private static bool? IsSpecialType(this ITypeSymbol? symbol)
+    internal static bool? IsSpecialType(this ITypeSymbol? symbol)
     {
         return symbol switch
         {
@@ -361,7 +189,7 @@ public static class PrepareData
         };
     }
 
-    private static string? GetNamedArgumentExpression(this AttributeSyntax attributeSyntax, string name)
+    internal static string? GetNamedArgumentExpression(this AttributeSyntax attributeSyntax, string name)
     {
         attributeSyntax = attributeSyntax ?? throw new ArgumentNullException(nameof(attributeSyntax));
 
@@ -407,7 +235,7 @@ public static class PrepareData
                attribute.GetNamedArgument(name).Value as ITypeSymbol;
     }
 
-    private static string? ExpandDefaultValueExpression(string? defaultValue, ITypeSymbol? typeSymbol)
+    internal static string? ExpandDefaultValueExpression(string? defaultValue, ITypeSymbol? typeSymbol)
     {
         if (typeSymbol == null ||
             defaultValue is not { Length: > 0 } ||
