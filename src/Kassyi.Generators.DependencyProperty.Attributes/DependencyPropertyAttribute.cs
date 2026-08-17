5240df13716cad5cab68fcb481c7f76a883682b2
kassyi <22139176+kassyi@users.noreply.github.com>
2026-08-18 05:09:13 +0900

    docs(attributes): correct XML documentation summary for DependencyPropertyAttribute
    - Details of changes:
      - Fixed typo in XML documentation summary for DependencyPropertyAttribute and DependencyPropertyAttribute<T> by replacing 'attached dependency property' with 'dependency property'.
    - Technical background:
      - Ensures accurate IntelliSense descriptions for non-attached dependency properties.

--- src/Kassyi.Generators.DependencyProperty.Attributes/DependencyPropertyAttribute.cs
+++ src/Kassyi.Generators.DependencyProperty.Attributes/DependencyPropertyAttribute.cs
@@ -9,7 +9,7 @@ using System.Diagnostics;
 
 namespace Kassyi.Generators.DependencyProperty;
 
-/// <summary>Generates attached dependency property using DependencyProperty.Register.</summary>
+/// <summary>Generates dependency property using DependencyProperty.Register.</summary>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 [Conditional("DEPENDENCY_PROPERTY_GENERATOR_ATTRIBUTES")]
 internal sealed class DependencyPropertyAttribute : Attribute
@@ -130,7 +130,7 @@ public DependencyPropertyAttribute(
     }
 }
 
-/// <summary>Generates attached dependency property using DependencyProperty.Register.</summary>
+/// <summary>Generates dependency property using DependencyProperty.Register.</summary>
 /// <typeparam name="T">Type of this dependency property.</typeparam>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 [Conditional("DEPENDENCY_PROPERTY_GENERATOR_ATTRIBUTES")]
